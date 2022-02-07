b50206c75db887b9da29cd6cca88ca0fb8b572ca
masimassimo <33621723+masimassimo@users.noreply.github.com>
2022-02-07 11:13:47 +0100

    Update __manifest__.py
    rimossa dipendenza fatturapa_sale

--- moduli_stampa_ph/__manifest__.py
+++ moduli_stampa_ph/__manifest__.py
@@ -15,7 +15,7 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.2',
+    'version': '0.3',
 
     # any module necessary for this one to work correctly
     'depends': ['base',
@@ -34,7 +34,6 @@
                 'l10n_it_fatturapa',
                 'l10n_it_fatturapa_in',
                 'l10n_it_fatturapa_out',
-                'l10n_it_fatturapa_sale',
                 'l10n_it_fiscal_document_type',
                 'l10n_it_fiscal_payment_term',
                 'l10n_it_fiscalcode',
